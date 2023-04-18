70d7a7307fc9186d51ac13998e71812fb776592c
Emil Korudzhiev <e.korudzhiev@abv.bg>
2023-04-18 11:20:59 +0300

    Shop update

--- controllers/store.php
+++ controllers/store.php
@@ -1,7 +1,7 @@
 <?php
 require('../components/header.inc.php');
 
-include('../components/emptyspace.inc.php');
+include('../components/shop-sidebar.inc.php');
 
 require('../components/footer.inc.php');
 ?>
